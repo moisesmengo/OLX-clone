ddb80683f2cf862f3a7e82a4d6ad0887ae04f697
Moisés Costa <moisesmengo@live.com>
2020-06-18 12:33:49 -0300

    criando slide

--- frontend/src/pages/AdPage/index.js
+++ frontend/src/pages/AdPage/index.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { PageArea, Fake } from './styled';
-import useApi from '../../helpers/OlxAPI';
+import { PageArea, Fake } from './styled'
+import useApi from '../../helpers/OlxAPI'
+import {Slide} from 'react-slideshow-image'
 
-import { PageContainer } from '../../components/MainComponents';
+import { PageContainer } from '../../components/MainComponents'
 
 const Page = () => {
     const api = useApi();
     const {id} = useParams()
 
     const [loading, setLoading] = useState([])
-    const [adInfo, setAdInfo] = useState([])
+    const [adInfo, setAdInfo] = useState({})
+
+    useEffect(() => {
+        const getAdInfo =  async (id) => {
+            const json =  await api.getAd(id, true)
+            setAdInfo(json)
+            setLoading(false)
+        }
+        getAdInfo(id)
+    }, [])
+
+    const formatDate = date =>{
+        let cDate = new Date(date)
+        let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 
+                    'julho', 'agosto', 'setempro', 'outubro', 'novembro', 'dezembro']
+        let cDay = cDate.getMonth()
+        let cMonth = cDate.getMonth()
+        let cYear = cDate.getFullYear()
+
+        return `${cDay} de ${months[cMonth]} de ${cYear}`
+    }
 
     return (
         <PageContainer>
@@ -19,13 +40,33 @@ const Page = () => {
                     <div className="box">
                         <div className="adImage">
                             {loading && <Fake height={300} />}
+                            {adInfo.images && 
+                                <Slide>
+                                    {adInfo.images.map((img,k) => 
+                                        <div key={k} className="each-slide">
+                                            <img src={img} alt=""/>
+                                        </div>
+                                    )}
+                                </Slide>
+                            }
                         </div>
                         <div className="adInfo">
                             <div className="adName">
                                 {loading && <Fake height={20}/>}
+                                {adInfo.title && 
+                                    <h2> {adInfo.title} </h2>
+                                }
+                                {adInfo.dateCreated && 
+                                   <small>Criado em {formatDate(adInfo.dateCreated)}</small>
+                                }
                             </div>
                             <div className="adDescription">
                                 {loading && <Fake height={100} />}
+                                {adInfo.description}
+                                <hr/>
+                                {adInfo.views &&
+                                    <small>Vizualizações: {adInfo.views} </small>
+                                }
                             </div>
                         </div>
                     </div>
